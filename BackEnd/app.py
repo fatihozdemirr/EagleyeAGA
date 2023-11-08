fe840d9ca4dbca6d81dfcffb585ce310082b215d
Fatih <fatihozdemir@3eendustriyel.com.tr>
2023-11-08 15:21:01 +0300

    WorkingProject

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -12,6 +12,7 @@ db = SQLAlchemy(app)
 db.create_all()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)   
     password = db.Column(db.String(120), nullable=False)
