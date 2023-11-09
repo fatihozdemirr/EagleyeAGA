f065444473167d268b9167541dda2e66e7d98898
Fatih <fatihozdemir@3eendustriyel.com.tr>
2023-11-09 12:24:06 +0300

    working on the phone

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -16,22 +16,29 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)   
     password = db.Column(db.String(120), nullable=False)
-    
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Register')
+# class registrationform(flaskform):
+#     username = stringfield('username', validators=[datarequired()])
+#     password = passwordfield('password', validators=[datarequired()])
+#     submit = submitfield('register')
+    
+logged_in = False
 
 @app.route('/')
-def home():
-    # if 'username' in session:        
-    #     return render_template('anasayfa.html')
+def ana_sayfa():
+    if logged_in:
+        return render_template('anasayfa.html')
+    return redirect(url_for('login'))
+
+@app.route('/anasayfa', methods=['GET', 'POST'])
+def anasayfa():
+    if logged_in:
+        return render_template('anasayfa.html')
     return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -43,36 +50,36 @@ def login():
 
         user = User.query.filter_by(username=username, password=password).first()
         if user:
-            #session['username'] = username
-            #flash('Giriş başarılı!', 'success')
+            global logged_in
+            logged_in = True
             return render_template('anasayfa.html')
         else:
             flash('Username or password is wrong!', 'danger')
 
     return render_template('login.html', form=form)
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
+# @app.route('/register', methods=['get', 'post'])
+# def register():
+#     form = registrationform()
+#     if form.validate_on_submit():
+#         username = form.username.data
+#         password = form.password.data
 
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
-            flash('Kullanıcı adı zaten kullanılıyor.' , 'danger')
-        else:
-            new_user = User(username=username, password=password)
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Kayıt başarılı! Şimdi giriş yapabilirsiniz.', 'success')
+#         existing_user = user.query.filter_by(username=username).first()
+#         if existing_user:
+#             flash('kullanıcı adı zaten kullanılıyor.' , 'danger')
+#         else:
+#             new_user = user(username=username, password=password)
+#             db.session.add(new_user)
+#             db.session.commit()
+#             flash('kayıt başarılı! şimdi giriş yapabilirsiniz.', 'success')
 
-    return render_template('register.html', form=form)
+#     return render_template('register.html', form=form)
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    if 'username' in session:
-        session.pop('username', None)
+    global logged_in
+    logged_in = False
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
